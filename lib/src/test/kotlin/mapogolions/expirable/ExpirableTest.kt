0cb07cf07d42265080591a9e2b64bf6cf04173a8
mapogolions <ivanovpavelalex45@gmail.com>
2023-06-17 02:40:11 +0500

    callback should be called on item expiration

--- lib/src/test/kotlin/mapogolions/expirable/ExpirableTest.kt
+++ lib/src/test/kotlin/mapogolions/expirable/ExpirableTest.kt
@@ -1,10 +1,15 @@
 package mapogolions.expirable
 
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 import kotlin.test.Test
-import kotlin.test.assertTrue
+import kotlin.test.assertEquals
 
 class ExpirableTest {
-    @Test fun someLibraryMethodReturnsTrue() {
-        assertTrue(true)
+    @Test fun shouldCallCallbackOnlyOnce_whenItemIsExpired() {
+        val latch = CountDownLatch(4)
+        Expirable("foo", Foo(), 50) { latch.countDown() }
+        latch.await(1, TimeUnit.SECONDS)
+        assertEquals(latch.count, 3)
     }
 }
