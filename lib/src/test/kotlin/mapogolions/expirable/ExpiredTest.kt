099e2a4df40a36a141cb4f4ab44f14a75de8ef8e
mapogolions <ivanovpavelalex45@gmail.com>
2023-06-17 01:08:35 +0500

    test scenario when object is alive

--- lib/src/test/kotlin/mapogolions/expirable/ExpiredTest.kt
+++ lib/src/test/kotlin/mapogolions/expirable/ExpiredTest.kt
@@ -2,14 +2,24 @@ package mapogolions.expirable
 
 import kotlin.test.Test
 import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class ExpiredTest {
-    @Test fun shouldBeAbleToTrackEndOfLifetime() {
+    @Test fun shouldBeAbleToDetectEndOfLifetime_whenThereIsNoReference() {
         val expired = expiredFactory(Foo())
         // anti-pattern
         var count = 100
         while (expired.alive && count-- > 0) System.gc()
         assertFalse(expired.alive)
+    }
+
+    @Test fun shouldBeAbleToDetectThatObjectIsAlive_whenThereIsAtLeastOneReachableReference() {
+        val obj = Foo()
+        val expired = expiredFactory(obj)
+        // anti-pattern
+        var count = 100
+        while (expired.alive && count-- > 0) System.gc()
+        assertTrue(expired.alive)
     }
 }
 
