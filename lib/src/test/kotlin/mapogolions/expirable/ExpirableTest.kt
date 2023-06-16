8ab4258523042f146822be61e729722b46e7eda6
mapogolions <ivanovpavelalex45@gmail.com>
2023-06-17 02:42:58 +0500

    refactoring

--- lib/src/test/kotlin/mapogolions/expirable/ExpirableTest.kt
+++ lib/src/test/kotlin/mapogolions/expirable/ExpirableTest.kt
@@ -4,12 +4,16 @@ import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertIs
 
 class ExpirableTest {
     @Test fun shouldCallCallbackOnlyOnce_whenItemIsExpired() {
-        val latch = CountDownLatch(4)
-        Expirable("foo", Foo(), 50) { latch.countDown() }
+        val latch = CountDownLatch(2)
+        Expirable("foo", Foo(), 50) {
+            assertIs<Foo>(it)
+            latch.countDown()
+        }
         latch.await(1, TimeUnit.SECONDS)
-        assertEquals(latch.count, 3)
+        assertEquals(latch.count, 1)
     }
 }
