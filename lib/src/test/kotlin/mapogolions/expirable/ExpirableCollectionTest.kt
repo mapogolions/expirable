86856be609b2e3cde024d4bf548b9a76446c8782
mapogolions <ivanovpavelalex45@gmail.com>
2023-06-18 02:18:29 +0500

    increase test coverage

--- lib/src/test/kotlin/mapogolions/expirable/ExpirableCollectionTest.kt
+++ lib/src/test/kotlin/mapogolions/expirable/ExpirableCollectionTest.kt
@@ -4,6 +4,7 @@ import mapogolions.expirable.internal.ExpirableHooksImpl
 import java.util.concurrent.CountDownLatch
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotSame
 import kotlin.test.assertSame
 
 class ExpirableCollectionTest {
@@ -25,19 +26,32 @@ class ExpirableCollectionTest {
 
     @Test fun getOrPut_shouldReturnTheSameObject_whenObjectIsNotExpired() {
         // arrange
-        val latch = CountDownLatch(1)
-        val hooks = object : ExpirableHooksImpl<String, Item>() {
-            override fun afterCleanup(expirables: ExpirableCollection<String, Item>) = latch.countDown()
-        }
-        val items = ExpirableCollection(defaultCleanupInterval = 500, hooks = hooks)
+        val items = ExpirableCollection<String, Item>(defaultCleanupInterval = 500)
         val factory: (String) -> Item = { Item(it) }
-        val ttl: Long = 2000
 
         // act
-        val a = items.getOrPut("foo", factory, ttl)
-        val b = items.getOrPut("foo", factory, ttl)
+        val a = items.getOrPut("foo", factory, ttl = 2000)
+        val b = items.getOrPut("foo", factory, ttl = 2000)
 
         // assert
         assertSame(a, b)
+    }
+
+    @Test fun getOrPut_shouldReturnDifferentObject_whenObjectIsExpired() {
+        // arrange
+        val latch = CountDownLatch(1)
+        val hooks = object : ExpirableHooksImpl<String, Item>() {
+            override fun onExpire(expirable: Expirable<String, Item>) = latch.countDown()
+        }
+        val items = ExpirableCollection(defaultCleanupInterval = 500, hooks = hooks)
+        val factory: (String) -> Item = { Item(it) }
+
+        // act
+        val a = items.getOrPut("foo", factory, ttl = 20)
+        latch.await()
+        val b = items.getOrPut("foo", factory, ttl = 20)
+
+        // assert
+        assertNotSame(a, b)
     }
 }
