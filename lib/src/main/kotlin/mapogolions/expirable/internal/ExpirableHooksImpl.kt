fb3d829acff90215d2686ef44a1940cf85b14bef
mapogolions <ivanovpavelalex45@gmail.com>
2023-06-17 20:16:30 +0500

    remove redundant modifier

--- lib/src/main/kotlin/mapogolions/expirable/internal/ExpirableHooksImpl.kt
+++ lib/src/main/kotlin/mapogolions/expirable/internal/ExpirableHooksImpl.kt
@@ -5,8 +5,8 @@ import mapogolions.expirable.ExpirableCollection
 import mapogolions.expirable.Expired
 
 internal open class ExpirableHooksImpl<K : Any, T> : ExpirableHooks<K, T> {
-    open override fun beforeCleanup(expirables: ExpirableCollection<K, T>) = Unit
-    open override fun afterCleanup(expirables: ExpirableCollection<K, T>) = Unit
-    open override fun onExpire(expirable: Expirable<K, T>) = Unit
-    open override fun onDequeue(expired: Expired<T>) = Unit
+    override fun beforeCleanup(expirables: ExpirableCollection<K, T>) = Unit
+    override fun afterCleanup(expirables: ExpirableCollection<K, T>) = Unit
+    override fun onExpire(expirable: Expirable<K, T>) = Unit
+    override fun onDequeue(expired: Expired<T>) = Unit
 }
