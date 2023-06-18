7f9160adaa78d9ee2c92d4e1aa2d08cccf3a557e
mapogolions <ivanovpavelalex45@gmail.com>
2023-06-18 17:38:21 +0500

    increase test coverage

--- lib/src/test/kotlin/mapogolions/expirable/ExpirableCollectionTest.kt
+++ lib/src/test/kotlin/mapogolions/expirable/ExpirableCollectionTest.kt
@@ -77,4 +77,25 @@ class ExpirableCollectionTest {
         assertEquals(items.size, 0)
         assertEquals(items.cleanupQueueSize, 0)
     }
+
+    @Test fun shouldBeAbleToUseObjectAfterExpiration_whenThereIsAtLeastOneReachableReference() {
+        // arrange
+        val latch = CountDownLatch(1)
+        val hooks = object : ExpirableHooksImpl<String, Item>() {
+            override fun onDequeue(expired: Expired<Item>) {
+                gc(hints = 200) { expired.alive }
+                latch.countDown()
+            }
+        }
+        val items = ExpirableCollection(defaultCleanupInterval = 500, hooks = hooks)
+
+        // act
+        val foo = items.getOrPut("foo", ::itemFactory, ttl = 20)
+        latch.await()
+
+        // assert
+        assertEquals(items.size, 0)
+        assertEquals(items.cleanupQueueSize, 1)
+        assertEquals(foo.name, "foo")
+    }
 }
